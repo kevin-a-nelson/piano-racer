21d6eaeb4ebcc855ba157725c209fe7a7491a714
Kevin <nelson67@stolaf.edu>
2021-12-31 03:15:31 -0600

    moving e

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import SheetMusic from '@slnsw/react-sheet-music';
+import logo from './images/staff.png';
 import './App.css';
 
 function App() {
+  const [xoffset, setXoffset] = useState(0);
+  const [yoffset, setYoffset] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [delta, setDelta] = useState(1);
+
+
+  function moveNoteLeft() {
+    const newXoffset = xoffset + delta;
+    setXoffset(newXoffset)
+  }
+
+  useEffect(() => {
+    let counter = 0;
+    const interval = setInterval(() => {
+      counter += delta;
+      if (counter > 800) {
+        setXoffset(0)
+        counter = 0;
+      } else {
+        setXoffset((xoffset) => xoffset + delta)
+      }
+    }, 5);
+
+      return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
+      {counter}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='music-notes'>
+          <div className='staff'>
+              𝄚𝄚𝄚𝄚𝄚𝄚𝄚𝄚𝄚𝄚
+            <div className='signature'>
+              𝄞
+            </div>
+          </div>
+          <div className='staff'>
+              𝄚𝄚𝄚𝄚𝄚𝄚𝄚𝄚𝄚𝄚
+            <div className='signature'>
+              𝄢
+            </div>
+            <div className='notes'>
+              <div className='note' style={{right: xoffset}}>
+                𝅝
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
